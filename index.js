3e2a58ded9d11b59a8aaecf830e084c44d742c68
Albertino Neto <albeneto72@gmail.com>
2021-06-21 17:28:10 -0300

    Organizando as rotas e instalando consign.

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ const app = express();
 
 app.listen(3000, ()=> console.log('Servidor rodando na porta 3000'));
 
-app.get('/atendimentos', (req, res)=> res.send('Você esta na rota de atendimentos.'));
