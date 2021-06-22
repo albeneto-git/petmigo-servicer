ad4606277bce5e07e77e9ea0d13a42d567356114
Albertino Neto <albeneto72@gmail.com>
2021-06-21 21:54:09 -0300

    Organização do Express e instalação body-parser

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,4 +3,13 @@ module.exports = app => {
     
     app.get('/atendimentos', (req, res)=> res.send('Você esta na rota de atendimentos e esta realizando um GET.'));
 
+    app.post('/atendimentos', (req, res) => {
+
+        console.log(req.body);
+        
+        res.send('Você esta na roda de atendimento e esta realizando um POST')
+
+    });
+        
+
 }
