2e2c30e9919321f070443e47b0c4c5e056d62a54
Albertino Neto <albeneto72@gmail.com>
2021-08-02 20:28:50 -0300

    Cria a tabela se não existir

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -7,7 +7,7 @@ class Tabelas{
     }
 
     criarAtendimentos(){
-        const sql = 'CREATE TABLE atendimentos (id INT NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20) NOT NULL, servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacao TEXT, PRIMARY KEY (id))';
+        const sql = 'CREATE TABLE IF NOT EXISTS atendimentos (id INT NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20) NOT NULL, servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacao TEXT, PRIMARY KEY (id))';
         this.conexao.query(sql, erro =>{
             if(erro){
                 console.log(erro);
