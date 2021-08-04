ceb7dd3e67a396dceb71ad6b47c9def6324c0b40
Albertino Neto <albeneto72@gmail.com>
2021-08-04 19:44:09 -0300

    Adicionando data e dataCriacao ao serviço e instalando a biblioteca moment.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,8 +2,11 @@ const conexao = require('../infraestrutura/conexao');
 class Atendimento{
 
     adiciona(atendimento){
+        
+        const dataCriacao = new Date();
+        const atendimentoDatado = {...atendimento, dataCriacao};
         const sql = 'INSERT INTO atendimentos SET ?';
-        conexao.query(sql, atendimento, (erro, resultados)=>{
+        conexao.query(sql, atendimentoDatado, (erro, resultados)=>{
 
             if(erro){
                 console.log(erro);
