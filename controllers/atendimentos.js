ebf8636a09561958206de2e431e3138d32a72b73
Albertino Neto <albeneto72@gmail.com>
2021-08-04 19:33:02 -0300

    Criação da rota post persistindo os dados via postman

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,13 +1,15 @@
-
+const Atendimento = require('../models/atendimentos');
 module.exports = app => {
     
     app.get('/atendimentos', (req, res)=> res.send('Você esta na rota de atendimentos e esta realizando um GET.'));
 
     app.post('/atendimentos', (req, res) => {
 
-        console.log(req.body);
+        const atendimento = req.body;
         
-        res.send('Você esta na roda de atendimento e esta realizando um POST')
+        Atendimento.adiciona(atendimento);
+
+        res.send('POST atendimento');
 
     });
         
