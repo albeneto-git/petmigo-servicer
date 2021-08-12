33861504ddf20c4740b04f5c7e6cdd259de5af9b
Albertino Neto <albeneto72@gmail.com>
2021-08-11 21:16:11 -0300

    Alteração de um atendimento por meio do PATH

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -17,6 +17,12 @@ module.exports = app => {
         Atendimento.adiciona(atendimento, res);
 
     });
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+        Atendimento.altera(id, valores, res);
+    })
         
 
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -67,6 +67,24 @@ class Atendimento{
         })
     }
 
+    altera(id, valores, res){
+
+        if(valores.data){
+            valores.data = moment(valores.data,'DD/MM/YYYY').format('YYYY-MM-DD hh:mm:ss');
+        }
+
+        const sql = 'UPDATE atendimentos SET ? WHERE id = ?';
+        conexao.query(sql, [valores, id], (erro, resultado) =>{
+
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(resultado);
+            }
+
+        });
+    }
+
 }
 
 module.exports = new Atendimento;
