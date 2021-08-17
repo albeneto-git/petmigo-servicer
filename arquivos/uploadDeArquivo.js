52c22e2a579db8fbbd99ee806eb2327eda89292a
Albertino Neto <albeneto72@gmail.com>
2021-08-16 21:53:37 -0300

    Manipulação de arquivos

--- arquivos/uploadDeArquivo.js
+++ arquivos/uploadDeArquivo.js
@@ -5,6 +5,10 @@ createReadStream - cria um stream
 pipe - pega o stream e repassa para o createWriteStream
 on - escuta um evento e dispara uma função.
 */
-fs.createReadStream('./assets/dog.jpg')
-    .pipe(fs.createWriteStream('./assets/dog_stream.jpg'))
-    .on('finish', () => console.log('imagem foi escrita com sucesso!'));
+
+module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
+    const novoCaminho  = `./assets/imagens/${nomeDoArquivo}`;
+    fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callBackImagemCriada(novoCaminho));
+}
--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,22 @@
 const conexao = require('../infraestrutura/conexao');
-
+const upLoadDeArquivo = require('../arquivos/uploadDeArquivo');
 class Pet{
 
     adiciona(pet, res){
         const sql = 'INSERT INTO pets SET ?';
-        console.log(pet);
-        conexao.query(sql, pet, erro => {
-            if(erro){
-                res.status(400).json(erro);
-            }else{
-                res.status(200).json(pet);
-            }
-        });
+        upLoadDeArquivo(pet.imagem, pet.nome, (novoCaminho)=>{
+            
+            const novoPet = {nome: pet.nome, imagem: novoCaminho};
+
+            conexao.query(sql, novoPet, erro => {
+                if(erro){
+                    res.status(400).json(erro);
+                }else{
+                    res.status(200).json(novoPet);
+                }
+            });
+
+        })
 
     }
 
