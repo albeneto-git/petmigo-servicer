673949287232a4645cc6e0d468b8ffae5250eaac
Albertino Neto <albeneto72@gmail.com>
2021-08-21 22:26:13 -0300

    Refatorando o Get

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -51,17 +51,8 @@ class Atendimento{
         }
     }
 
-    lista(res){
-        const sql = 'SELECT * FROM atendimentos';
-
-        conexao.query(sql, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro);
-            }else{
-                res.status(200).json(resultados);
-            }
-        })
-
+    lista(){
+        return repositorio.lista();
     }
 
     buscaPorId(id, res){
